317e4b3d793873b7b79f8ca14e598d40f99bf5a6
SunghaEom <doubleb@kaist.ac.kr>
2018-05-07 20:33:55 +0900

    vm/frame.c, frame.h added

---
+++ src/vm/frame.c
@@ -0,0 +1,56 @@
+#include "threads/palloc.h"
+#include "threads/synch.h"
+#include "vm/page.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <list.h>
+
+
+void frame_table_init (void);
+void frame_add_to_table (void *frame, struct supple_page_entry *spte)
+
+void frame_table_init (void)
+{
+  lock_init (&frame_table_lock);
+  list_init (&frame_table);
+}
+
+void frame_add_to_table (void *frame, struct supple_page_entry *spte)
+{
+  struct frame_entry *fte = malloc (sizeof (struct frame_entry));
+  fte-> frame = frame;
+  fte-> spte = spte;
+  fte-> thread = thread_current();
+  lock_acquire (&frame_table_lock);
+  list_push_back (&frame_table, &fte->elem);
+  lock_release (&frame_table_lock);
+}
+
+void* frame_alloc (enum palloc_flags flag, struct supple_page_entry *spte)
+{
+  if ( (flags & PAL_USER ) == 0 )
+  {
+    return NULL;
+  }
+  void *frame = palloc_get_page (flag);
+  if (frame)
+  {
+    frame_add_to_table (frame, spte);
+  }
+  else
+  {
+    while (!frame)
+    {
+      lock_acquire (&frame_table_lock);
+      frame = frame_evict (flag);
+      lock_release (&frame_table_lock);
+    }
+    frame_add_to_table (frame, spte);
+  }
+  return frame;
+}
+
+void frame_evict (enum palloc_flags flag)
+{
+  struct list_elem *e = list_begin (&frame_table);
+}
