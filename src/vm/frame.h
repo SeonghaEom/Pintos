03eed5a7b6d77053cc7dd11f8aaeee8a5f82deed
kimyoonseo <kimyoonseo@kaist.ac.kr>
2018-05-12 05:19:53 +0900

    frame.h 지워져 있던거 복구

---
+++ src/vm/frame.h
@@ -0,0 +1,35 @@
+#ifdef VM_FRAME_H
+#define VM_FRAME_H
+
+#include <list.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "threads/palloc.h"
+#include "threads/synch.h"
+#include "vm/page.h"
+
+/*
+ * 2018.05.05
+ * KimYoonseo
+ * EomSungha
+ */
+/* Frame table lock */
+struct lock frame_table_lock;
+/* Global frame table */
+struct list frame_table;
+/* Frame table entry */
+struct frame_table_entry
+{
+  void *frame;              /* Physical frame pointer */
+  struct spte *spte;        /* Supplement page entry */
+  struct thread *thread;    /* Thread who uses this frame */
+  struct list_elem elem;    /* List element */
+};
+
+void frame_table_init (void);
+void frame_add_to_table (void *frame, struct spte *spte);
+void *frame_alloc (enum palloc_flags flags, struct spte *spte);
+void frame_free (void *frame);
+void *frame_evict (enum palloc_flags flags);
+
+#endif
